118ad9da4a8647f228741a9b2425ca6f3400ae46
Philippe Perret <philippe.perret@yahoo.fr>
2018-05-02 06:01:03 +0300

    Remplacement nom reponses -> sequence_keys

--- tests_cli/lib/required/Tests/class.rb
+++ tests_cli/lib/required/Tests/class.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 class Tests
   ERRORS = {
-    'reponses-required' => 'Pour tester les programmes CLI, il faut définir les réponses successives en les définissant par `Tests.sequence_touches=[\'réponse 1\', \'réponse 2\', etc.]` (dans l’ordre de leur utilisation).',
+    'sequence_keys-required' => 'Pour tester les programmes CLI, il faut définir les réponses successives en les définissant par `Tests.sequence_touches=[\'réponse 1\', \'réponse 2\', etc.]` (dans l’ordre de leur utilisation).',
     'no-more-reponse'   => 'Il ne reste plus de réponse à prendre. Impossible de poursuivre.'
   }
 
@@ -11,7 +11,7 @@ class << self
   def init
     # Création du dossier contenant les fichier des tests, au besoin
     `mkdir -p "#{folder}"`
-    File.exist?(path_reponses_file) && File.unlink(path_reponses_file)
+    File.exist?(path_sequence_keys_file) && File.unlink(path_sequence_keys_file)
     Log.init
     Messages.init
   end
@@ -20,9 +20,9 @@ class << self
   # séquence de touches donnée au test.
   def next_touche options = nil
     options ||= Hash.new
-    Tests::Log << "-> next_touche (réponses restantes : #{reponses.inspect})"
-    reponses.empty? && raise('no-more-reponse')
-    rep = reponses.shift
+    Tests::Log << "-> next_touche (réponses restantes : #{sequence_keys.inspect})"
+    sequence_keys.empty? && raise('no-more-reponse')
+    rep = sequence_keys.shift
 
     # Réponse temporisée
     rep.is_a?(Array) && begin
@@ -35,7 +35,7 @@ class << self
     # un true/false/nil
     rep = rep.to_s
 
-    Tests::Log << "Réponse retournée pour #{options[:from].sans_couleur} : #{rep.inspect} (nouvelles réponses restants : #{reponses.inspect})"
+    Tests::Log << "Réponse retournée pour #{options[:from].sans_couleur} : #{rep.inspect} (nouvelles réponses restants : #{sequence_keys.inspect})"
     rep
   rescue Exception => e
     ERRORS.key?(e.message) && Tests::Log.error(ERRORS[e.message])
@@ -46,8 +46,8 @@ class << self
   # Les réponses définies dans les tests
   def sequence_touches
     @sequence_touches ||= begin
-      File.exist?(path_reponses_file) || raise('reponses-required')
-      File.read(path_reponses_file).force_encoding('utf-8').split("\n")
+      File.exist?(path_sequence_keys_file) || raise('sequence_keys-required')
+      File.read(path_sequence_keys_file).force_encoding('utf-8').split("\n")
     end
   end
   alias :sequence_keys :sequence_touches
@@ -55,13 +55,13 @@ class << self
   # Pour définir les réponses à simuler
   def sequence_touches= value
     value.is_a?(Array) || raise('Tests.sequence_touches= attend une liste de réponses, même s’il y a une seule réponse.')
-    File.exist?(path_reponses_file) && File.unlink(path_reponses_file)
-    File.open(path_reponses_file,'wb'){|f|f.write(value.join("\n"))}
+    File.exist?(path_sequence_keys_file) && File.unlink(path_sequence_keys_file)
+    File.open(path_sequence_keys_file,'wb'){|f|f.write(value.join("\n"))}
   end
   alias :sequence_keys= :sequence_touches=
 
-  def path_reponses_file
-    @path_reponses_file ||= File.join(folder, 'reponses')
+  def path_sequence_keys_file
+    @path_sequence_keys_file ||= File.join(folder, 'sequence_keys')
   end
 
   # Le dossier des tests
